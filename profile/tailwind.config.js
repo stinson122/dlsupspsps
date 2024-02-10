6f7eb2e24188be672e457bc4f43fdc741d488b4f
stinson122 <audrey_stinson@dlsu.edu.ph>
2024-02-11 00:24:46 +0800

    edit-profile

---
+++ profile/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                'red-maroon': '#38000b',
+                'olive': '#716d38',
+                'beige': '#e7ddd3',
+                'burnt-sienna': '#b24f32',
+                'dim-gray': '#696969',
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+}
+
+
